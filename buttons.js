95e7433c2b77ee56b784dc94bf24b805835a72cc
cruzcstll99 <42954446+cruzcstll99@users.noreply.github.com>
2026-04-10 11:02:12 -0500

    Initial commit

---
+++ buttons.js
@@ -0,0 +1,13 @@
+const intro_gifs = ['button/elliptic.png','button/PCSv3.gif'];
+const intro_text =[ 'Non-vanishing of analytic ranks [1]','Pair correlation surface, see [4]'];
+let ind = 0;
+
+function switchintro() {
+    ind = (ind +1) % intro_gifs.length;
+    document.getElementById("intro_text").innerHTML = intro_text[ind];
+    document.getElementById("intro_pic").src = intro_gifs[ind];
+}
+function email() {
+    navigator.clipboard.writeText("ccasti30@illinois.edu");
+    alert("The email address of Cruz Castillo has been copied to your clipboard!");
+}
